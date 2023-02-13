01bb84d68a65528adf2cb0646a85b58cf7b49164
kuhakuX <112263004+kuhakuX@users.noreply.github.com>
2023-02-13 12:20:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 ''' 
-READ ME ZONE
+              _ . - * ` READ ME ZONE ` * - . _
+               
 - - - - - - - - - - I M P O R T A N T - - - - - - - - -
-NEED TO INSTALL python 
-NEED TO INSTALL " pillow " package --------------->  "pip install pillow"
+
+NEED TO INSTALL PYTHON ------------------------------> https://www.python.org/downloads/
+
+# # # if you use  my code but not install PYTHON i will slap you ass lol just kidding # # #
+# https://media.tenor.com/b_4ERajyrPEAAAAC/slap-butts-anime.gif #
+
+NEED TO INSTALL " pillow " package ------------------>  "pip install pillow"
 NEED TO INSTALL " pyautogui " package --------------->  "pip install pyautogui"
-NEED TO INSTALL " keyboard " package --------------->  "pip install keyboard"
+NEED TO INSTALL " keyboard " package ---------------->  "pip install keyboard"
+
+find your x , y by your self cuz im lazy to explan about x , y lol  Don't Angry at me Onii-chan i'm just a lazy cat
 
 - - - - - - - - - - I M P O R T A N T - - - - - - - - -
-END READ ME ZONE
+# Onii-chan HENTAI - 3 - b #
+                  
+             ` * - . _ END READ ME ZONE _ . - * `
 '''
 import keyboard
 import pyautogui
