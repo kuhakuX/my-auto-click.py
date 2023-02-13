0faf5a190e7f3fb486e4773702d0d761d052968a
kuhakuX <112263004+kuhakuX@users.noreply.github.com>
2023-02-13 12:02:34 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import keyboard
+import pyautogui
+while keyboard.is_pressed('x')==False:
+    pyautogui.click(x=34, y=38)
+    pyautogui.click(x=99, y=37)
+    pyautogui.click(x=196, y=38)
+    pyautogui.click(x=37, y=147)
+    pyautogui.click(x=196, y=138)
